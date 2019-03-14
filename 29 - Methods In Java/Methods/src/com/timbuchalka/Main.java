402827ec2c68b429bfeaf296f7bb01ee8be37b27
Ivan Voitovych <noctua.vt@gmail.com>
2019-03-14 18:36:23 +0200

    First commit

---
+++ 29 - Methods In Java/Methods/src/com/timbuchalka/Main.java
@@ -0,0 +1,35 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        gameOver = false;
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        System.out.println(calculateScore(gameOver, score, levelCompleted, bonus));
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }
+
+        return -1;
+
+    }
+
+}
